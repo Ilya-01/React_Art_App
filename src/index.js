7c8e4de0c326993a5fbe83a272231ce0bc2d2ca5
Ilya Panarin <panarinila82@gmail.com>
2021-12-29 02:08:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <h1>Art And Painting</h1>,
+  document.querySelector("[class=\"generalText\"]")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <img src="/Img/Art.jpg" alt="Error"/>,
+  document.querySelector("[class=\"image\"]")
+);
+
+ReactDOM.render(
+  <h5>You can visit the gallery to see more paintings and works of art.</h5>,
+  document.querySelector("[class=\"text\"]")
+);
+
